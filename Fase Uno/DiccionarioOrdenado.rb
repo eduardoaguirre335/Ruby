e5494762c4b0b7930c18865fcce32e9cb9d87dda
Eduardo <eduardoaguirre335@gmail.com>
2015-09-24 14:11:39 -0500

    DiccionarioOrdenado terminado

--- Fase Uno/DiccionarioOrdenado.rb
+++ Fase Uno/DiccionarioOrdenado.rb
@@ -1,24 +1,40 @@
 class DiccionarioOrdenado
-	attr_accessor :diccionario
+	#attr_accessor :diccionario
+	@@diccionario = []
+	@@contador = 0
 	def initialize()
-		puts "Escribe una palabra"
-		palabra = gets.chomp
-		usuario(palabra)
+		
 	end
-	
-	def usuario(palabra)
+
+	def usuario
+		puts @@diccionario
+		puts "Escribe una palabra"
+		palabra = gets.chomp.to_s
 		if palabra.length > 0
-			
+			@@diccionario << palabra
+			#puts @@diccionario
+			@@contador += 1
+			puts "Escribe otra palabra (o presiona enter para finalizar)"
+			palabra = gets.chomp.to_s
+			while palabra.length > 0
+				@@diccionario << palabra
+				@@contador += 1
+				#puts @@diccionario
+				puts "Escribe otra palabra (o presiona enter para finalizar)"
+				palabra = gets.chomp.to_s
+			end
+			orden
 		else
-			puts "No escribiste nada"
+			puts "No se escribio nada"
 		end
 	end
 
-	def otraPalabra
-		puts "Escribe otra palabra (o presiona enter para finalizar)"
-		
+	def orden
+		puts "Felicidades! Tu diccionario tiene #{@@contador} palabras"
+		puts @@diccionario.sort
 	end
 	
 end
 
 orden = DiccionarioOrdenado.new
+orden.usuario
